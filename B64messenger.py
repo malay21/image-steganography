0139aca7647881c3d06dddba7d7d296f9ecbbff2
malay21 <malayrpatil21@gmail.com>
2020-06-29 01:24:06 +0530

    encoding GUI with image select complete

--- B64messenger.py
+++ B64messenger.py
@@ -32,36 +32,70 @@ class imgstegno:
         bws_button = Button(df, text='path', command=lambda :self.d_path(df))
         bws_button.grid()
         df.grid(row=1)
-        back_button = Button(df, text='home', command= lambda :self.home(df))
+        back_button = Button(df, text='cancel', command= lambda :self.home(df))
         back_button.grid()
     
     #(GUI function)function for creating page after pressing encode button
     def encode_page(self,f):
         f.destroy()
         ef = Frame(root)
-        l1= Label(ef,text='Give the path of your image:')
-        l1.grid(pady=30)
-        bws_button = Button(ef,text='path',command=lambda : self.e_path(ef))
-        bws_button.grid()
+        l1= Label(ef,text='Select your image:')
+        l1.config(font=('courier',18))
+        l1.grid(pady=15)
+        bws_button = Button(ef,text='select',command=lambda : self.e_path(ef))
+        bws_button.config(font=('courier',16))
+        bws_button.grid(pady=15)
         ef.grid(row=1)
-        back_button = Button(ef, text='home', command= lambda :self.home(ef))
+        back_button = Button(ef, text='cancel', command=lambda :self.home(ef))
         back_button.grid()
+        back_button.config(font=('courier',16))
+
+    def e_path(self,ef):       
+        ep= Frame(root)
+        myfile = tkinter.filedialog.askopenfilename(filetypes = ([('Image Files', '*.png *.jpg')]))
+        myimg = Image.open(myfile)
+        myimage = myimg.resize((250,150))
+        ef.destroy()
+        img = ImageTk.PhotoImage(myimage)
+        l3= Label(ep,text='selected image')
+        l3.grid()
+        panel = Label(ep, image=img)
+        panel.image = img
+        self.o_image_size = os.stat(myfile)
+        self.o_image_w, self.o_image_h = myimg.size
+        panel.grid()
+        l2 = Label(ep, text='Enter the message')
+        l2.config(font=('courier',18))
+        l2.grid(pady=15)
+        text_area = Text(ep, width=50, height=10)
+        text_area.grid()
+        encode_button = Button(ep, text='Encode', command=lambda :self.enc_fun(text_area,myimg))
+        encode_button.config(font=('courier',16))
+        back_button = Button(ep, text='<--', command=lambda : self.home(ep))
+        back_button.config(font=('courier',16))
+        back_button.grid(pady=15)
+        encode_button.grid()
+        ep.grid(row=1)
+
+
+
     #(GUI function)front page
     def main(self,root):
         o_image_size=0
         root.title('Bmessenger')
-        root.geometry('400x400')
+        root.geometry('400x600')
         root.resizable(width =False, height=False)
         f = Frame(root)
         
         title = Label(f,text='Bmessenger')
-        title.config(font=('courier',20))
-        title.grid(pady=30)
+        title.config(font=('courier',18))
+        title.grid(pady=20)
 
         b_encode = Button(f,text='encode',command= lambda :self.encode_page(f))
-        b_encode.config(font=('courier',18))
+        b_encode.config(font=('courier',16))
+        b_encode.grid(pady=15)
         b_decode = Button(f,text='decode', command=lambda :self.decode_page(f))
-        b_decode.config(font=('courier',18))
+        b_decode.config(font=('courier',16))
 
         root.grid_rowconfigure(1, weight=1)
         root.grid_columnconfigure(0, weight=1)

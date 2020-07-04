9d3739a42ed5f9857d20d23cb96240fb99b7ded3
malay21 <malayrpatil21@gmail.com>
2020-07-04 21:48:26 +0530

    encryption completed

--- B64messenger.py
+++ B64messenger.py
@@ -93,8 +93,79 @@ class imgstegno:
             messagebox.showinfo("error","no text found! ")
         else:
             messagebox.showinfo("encrypted text",data)
+            newimg = myimg.copy()
+            self.encode_enc(newimg, data)
+            my_file = BytesIO()
+            newimg.save('o.png')
+            newimg.save(my_file,'png')
+            self.d_image_size = my_file.tell()
+            self.d_image_w,self.d_image_h = newimg.size
+            messagebox.showinfo("Success","Encoding Successful")
+    
+    def modPix(self,pix, data):
 
-            
+        datalist = self.genData(data)
+        lendata = len(datalist)
+        imdata = iter(pix)
+
+        for i in range(lendata):
+
+            # Extracting 3 pixels at a time
+            pix = [value for value in imdata.__next__()[:3] +
+                   imdata.__next__()[:3] +
+                   imdata.__next__()[:3]]
+
+            # Pixel value should be made
+            # odd for 1 and even for 0
+            for j in range(0, 8):
+                if (datalist[i][j] == '0') and (pix[j] % 2 != 0):
+
+                    if (pix[j] % 2 != 0):
+                        pix[j] -= 1
+
+                elif (datalist[i][j] == '1') and (pix[j] % 2 == 0):
+                    pix[j] -= 1
+
+            # Eigh^th pixel of every set tells
+            # whether to stop or read further.
+            # 0 means keep reading; 1 means the
+            # message is over.
+            if (i == lendata - 1):
+                if (pix[-1] % 2 == 0):
+                    pix[-1] -= 1
+            else:
+                if (pix[-1] % 2 != 0):
+                    pix[-1] -= 1
+
+            pix = tuple(pix)
+            yield pix[0:3]
+            yield pix[3:6]
+            yield pix[6:9]
+
+    def genData(self,data):
+
+        # list of binary codes
+        # of given data
+        newd = []
+
+        for i in data:
+            newd.append(format(ord(i), '08b'))
+        return newd
+                
+    def encode_enc(self,newimg, data):
+        w = newimg.size[0]
+        (x, y) = (0, 0)
+
+        for pixel in self.modPix(newimg.getdata(), data):
+
+            # Putting modified pixels in the new image
+            newimg.putpixel((x, y), pixel)
+            if (x == w - 1):
+                x = 0
+                y += 1
+            else:
+                x += 1
+
     #(GUI function)front page
     def main(self,root):
         o_image_size=0

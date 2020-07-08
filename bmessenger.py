3f2903744a35c14f80c0db50768e7401cdedce96
malay21 <malayrpatil21@gmail.com>
2020-07-08 12:10:56 +0530

    initiated main frame

--- bmessenger.py
+++ bmessenger.py
@@ -7,3 +7,35 @@ from PIL import Image
 from tkinter import messagebox
 from io import BytesIO
 import  os
+
+class frames:
+    def main(self,root):
+        o_image_size=0
+        root.title('Bmessenger')
+        root.geometry('400x600')
+        root.resizable(width =False, height=False)
+        #root.minsize(width =400, height=500)  #if you want to make it resizable
+        f = Frame(root)
+        title = Label(f,text='Bmessenger')
+        title.config(font=('courier',18))
+        title.grid(pady=20)
+
+        b_encode = Button(f,text='encode',command= lambda :self.encode_page(f))
+        b_encode.config(font=('courier',16))
+        b_encode.grid(pady=15)
+        b_decode = Button(f,text='decode', command=lambda :self.decode_page(f))
+        b_decode.config(font=('courier',16))
+
+        root.grid_rowconfigure(1, weight=1)
+        root.grid_columnconfigure(0, weight=1)
+        f.grid(row =1)
+        b_encode.grid()
+        b_decode.grid()
+
+
+
+   
+root=Tk()
+x=frames()
+x.main(root)
+root.mainloop()

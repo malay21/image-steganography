b7302155d0f77c652d12586a3795b096fcb141b3
malay21 <malayrpatil21@gmail.com>
2020-07-08 17:18:35 +0530

    error handling fixing for encode modules

--- bmessenger.py
+++ bmessenger.py
@@ -93,7 +93,7 @@ class func:
             text_area.grid()
             encode_button = Button(ep, text='cancel', command=lambda : frames.home(self,ep))
             encode_button.config(font=('courier',16))
-            back_button = Button(ep, text='Encode', command=lambda : func.e_fun(self,text_area,myimg))
+            back_button = Button(ep, text='Encode', command=lambda : [func.e_fun(self,text_area,myimg),frames.home(self,ep)])
             back_button.config(font=('courier',16))
             back_button.grid(pady=15)
             encode_button.grid()
@@ -108,14 +108,82 @@ class func:
             messagebox.showinfo("error","no text found! ")
         else:
             newimg = myimg.copy()
-            self.encode_enc(newimg, data)
+            temp =os.path.basename(myimg.filename)
+            func.encode_enc(self,newimg, data)
             my_file = BytesIO()
-            newimg.save(tkinter.filedialog.asksaveasfilename(filetypes = (('png', '*.png')),defaultextension=".png"))
-            newimg.save(my_file,'png')
-            self.d_image_size = my_file.tell()
-            self.d_image_w,self.d_image_h = newimg.size
-            messagebox.showinfo("Success","Encoding Successful")
+            newimg.save(tkinter.filedialog.asksaveasfilename(initialfile=temp,filetypes = ([('png', '*.png')]),defaultextension=".png"))
+            #newimg.save('output.png')              #test
+            if not newimg:
+                messagebox.showerror("error","you have selected nothing !")
+            else:
+                newimg.save(my_file,'png')
+                self.d_image_size = my_file.tell()
+                self.d_image_w,self.d_image_h = newimg.size
+                messagebox.showinfo("Success","Encoding Successful")
 
+    def encode_enc(self,newimg,data):
+        w = newimg.size[0]
+        (x, y) = (0, 0)
+
+        for pixel in func.modPix(self,newimg.getdata(), data):
+
+            # Putting modified pixels in the new image
+            newimg.putpixel((x, y), pixel)
+            if (x == w - 1):
+                x = 0
+                y += 1
+            else:
+                x += 1
+    
+    def modPix(self,pix,data):
+
+        datalist = func.genData(self,data)
+        lendata = len(datalist)
+        imdata = iter(pix)
+
+        for i in range(lendata):
+
+            # Extracting 3 pixels at a time
+            pix = [value for value in imdata.__next__()[:3] +
+                   imdata.__next__()[:3] +
+                   imdata.__next__()[:3]]
+
+            # Pixel value should be made
+            # odd for 1 and even for 0
+            for j in range(0, 8):
+                if (datalist[i][j] == '0') and (pix[j] % 2 != 0):
+
+                    if (pix[j] % 2 != 0):
+                        pix[j] -= 1
+
+                elif (datalist[i][j] == '1') and (pix[j] % 2 == 0):
+                    pix[j] -= 1
+
+            # Eigh^th pixel of every set tells
+            # whether to stop or read further.
+            # 0 means keep reading; 1 means the
+            # message is over.
+            if (i == lendata - 1):
+                if (pix[-1] % 2 == 0):
+                    pix[-1] -= 1
+            else:
+                if (pix[-1] % 2 != 0):
+                    pix[-1] -= 1
+
+            pix = tuple(pix)
+            yield pix[0:3]
+            yield pix[3:6]
+            yield pix[6:9]
+    
+    def genData(self,data):
+
+        # list of binary codes
+        # of given data
+        newd = []
+
+        for i in data:
+            newd.append(format(ord(i), '08b'))
+        return newd
 root=Tk()
 x=frames()
 x.main(root)

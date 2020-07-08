6f0f01fe70c59c486a42fcdce7209d9d6b0e92c3
malay21 <malayrpatil21@gmail.com>
2020-07-08 12:09:28 +0530

    import required library

---
+++ bmessenger.py
@@ -0,0 +1,9 @@
+import base64
+from tkinter import *
+from tkinter import ttk
+import tkinter.filedialog
+from PIL import ImageTk
+from PIL import Image
+from tkinter import messagebox
+from io import BytesIO
+import  os
